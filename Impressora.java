28f6de2d4b2fa1e080252f24c4ea7db45bd71aa6
unknown <aluno@L1M01.IPIRANGA.FATEC>
2022-09-22 17:41:43 -0300

    Classe Impressora criada

---
+++ Impressora.java
@@ -0,0 +1,21 @@
+public class Impressora{
+
+    public void exibir(float f){
+        System.out.println(f);
+    }
+    public void exibir(float f1, float f2){
+        System.out.println(f1 + " " + f2);
+    }
+    public void exibir(float f, String s){
+        System.out.println(f + " " + s);
+    }
+    public void exibir(String s, float f){
+        System.out.println(s + " " + f);
+    }
+    public void exibir(String s1, String s2, String s3){
+        System.out.println(s1 + " " + s2 + " " + s3);
+    }
+    public void exibir(String i1, int i2, String s){
+        System.out.println(i1 + " " + i2 + " " + s);
+    }
+}
